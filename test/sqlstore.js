5f590d12e57ef1ed5551e9491c6ddc32a0134a23
npow <nissan.pow@gmail.com>
2016-05-11 23:22:04 +0000

    added sequelize store

---
+++ test/sqlstore.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var Queue = require('../lib/queue');
+var SqlStore = require('../lib/stores/sql');
+
+describe('Store Usage', function() {
+  it('sql store should queue length', function (done) {
+    var s = new SqlStore();
+    var q = new Queue(function (n, cb) {
+      if (n === 1) {
+        assert.equal(q.length, 0);
+      }
+      cb();
+      done();
+    }, { store: s, autoResume: false })
+    q.push(1);
+  });
+});
