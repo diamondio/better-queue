d65b87e1f0a16ee881b80b4a23b43e42e0bf59ce
npow <nissan.pow@gmail.com>
2016-05-13 18:55:38 +0000

    destroy should pause workers

--- test/lib/helper.js
+++ test/lib/helper.js
@@ -7,9 +7,9 @@ mockery.registerMock('./SqliteAdapter', require('../fixtures/SqliteAdapter'));
 exports.destroyQueues = function (done) {
   async.each([this.q, this.q1, this.q2], function (q, qCB) {
     if (!q) return qCB();
-    setImmediate(function () {
+    setTimeout(function () {
       q.destroy(qCB);
-    });
+    }, 5);
   }, function (err) {
     if (err) console.error(err);
     done();
